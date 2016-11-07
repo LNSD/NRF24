28e2961a77eb6b2d0d221708b80392c7ab87dc4d
Lorenzo Delgado Antequera <lorenzo.delgado@lnsd.es>
2016-11-07 01:46:48 +0100

    Create Bitfield Union Typedefs for all registers

--- src/lib/NRF24.h
+++ src/lib/NRF24.h
@@ -23,11 +23,210 @@
 namespace NRF24
 {
     /**
-     * @name Driver Constants
+     * @name Constants
      */
 
     const static uint8_t MAX_PAYLOAD_SIZE = 32;
     const static uint8_t MAX_RF_CHANNEL = 127;
+
+    /**
+     * @name Register definitions
+     */
+
+    namespace Register
+    {
+        /**
+         * Configuration register
+         */
+        typedef union {
+            struct {
+                bool PRIM_RX     :1;
+                bool PWR_UP      :1;
+                bool CRCO        :1;
+                bool EN_CRC      :1;
+                bool MASK_MAX_RT :1;
+                bool MASK_TX_DS  :1;
+                bool MASK_RX_DR  :1;
+                bool             :1; /* reserved */
+            };
+            uint8_t raw;
+        } CONFIG;
+
+        /**
+         * Enable 'Auto Acknowledgement' Function
+         * @note Disable this functionality to be compatible with nRF2401
+         */
+        typedef union {
+            struct {
+                bool ENAA_P0 :1;
+                bool ENAA_P1 :1;
+                bool ENAA_P2 :1;
+                bool ENAA_P3 :1;
+                bool ENAA_P4 :1;
+                bool ENAA_P5 :1;
+                bool         :2; /* reserved */
+            };
+            uint8_t raw;
+        } EN_AA;
+
+        /**
+         * Enable RX Address
+         */
+        typedef union {
+            struct {
+                bool ERX_P0 :1;
+                bool ERX_P1 :1;
+                bool ERX_P2 :1;
+                bool ERX_P3 :1;
+                bool ERX_P4 :1;
+                bool ERX_P5 :1;
+                bool        :2; /* reserved */
+            };
+            uint8_t raw;
+        } EN_RXADDR;
+
+        /**
+         * Setup of Address Widths (common for all data pipes)
+         */
+        typedef union {
+            struct {
+                unsigned int AW :2;
+                unsigned int    :6; /* reserved */
+            };
+            uint8_t raw;
+        } SETUP_AW;
+
+        /**
+         * Setup of Automatic Retransmission
+         */
+        typedef union {
+            struct {
+                unsigned int ARD :4;
+                unsigned int ARC :4;
+            };
+            uint8_t raw;
+        } SETUP_RETR;
+
+        /**
+         * RF Channel
+         */
+        typedef union {
+            struct {
+                unsigned int RF_CH :7;
+                bool               :1; /* reserved */
+            };
+            uint8_t raw;
+        } RF_CH;
+
+        /**
+         * RF Setup Register
+         */
+        typedef union {
+            struct {
+                bool LNA_HCURR      :1; /* obsolete (nRF24L01 bit mnemonic) */
+                unsigned int RF_PWR :2;
+                bool RF_DR_HIGH     :1;
+                bool PLL_LOCK       :1;
+                bool RF_DR_LOW      :1;
+                bool                :1; /* reserved */
+                bool CONT_WAVE      :1;
+            };
+            uint8_t raw;
+        } RF_SETUP;
+
+        /**
+         * Status Register
+         */
+        typedef union {
+            struct {
+                bool TX_FULL         :1;
+                unsigned int RX_P_NO :3;
+                bool MAX_RT          :1;
+                bool TX_DS           :1;
+                bool RX_DR           :1;
+                bool                 :1; /* reserved */
+            };
+            uint8_t raw;
+        } STATUS;
+
+        /**
+         * Transmit observe register
+         */
+        typedef union {
+            struct {
+                unsigned int ARC_CNT  :4;
+                unsigned int PLOS_CNT :4;
+            };
+            uint8_t raw;
+        } OBSERVE_TX;
+
+        /**
+         * Received Power Detector (Carrier Detect)
+         */
+        typedef union {
+            struct {
+                bool RPD :1;
+                bool     :7; /* reserved */
+            };
+            uint8_t raw;
+        } RPD;
+
+        /**
+         * Rx Pipe Payload Width
+         */
+        typedef union {
+            struct {
+                unsigned int RX_PW_PN :6;
+                bool                  :2; /* reserved */
+            };
+            uint8_t raw;
+        } RX_PW_PN;
+
+        /**
+         * FIFO Status Register
+         */
+        typedef union {
+            struct {
+                bool RX_EMPTY :1;
+                bool RX_FULL  :1;
+                bool          :2; /* reserved */
+                bool TX_EMPTY :1;
+                bool TX_FULL  :1;
+                bool TX_REUSE :1;
+                bool          :1; /* reserved */
+            };
+            uint8_t raw;
+        } FIFO_STATUS;
+
+        /**
+         * Enable dynamic payload length
+         */
+        typedef union {
+            struct {
+                bool DPL_P0 :1;
+                bool DPL_P1 :1;
+                bool DPL_P2 :1;
+                bool DPL_P3 :1;
+                bool DPL_P4 :1;
+                bool DPL_P5 :1;
+                bool        :2; /* reserved */
+            };
+            uint8_t raw;
+        } DYNPD;
+
+        /**
+         * Feature Register
+         */
+        typedef union {
+            struct {
+                bool EN_DYN_ACK :1;
+                bool EN_ACK_PAY :1;
+                bool EN_DPL     :1;
+                bool            :5; /* reserved */
+            };
+            uint8_t raw;
+        } FEATURE;
+    }
 
     /**
      * @name Enum definitions
@@ -95,7 +294,7 @@ namespace NRF24
     } FifoStatus;
 
     /**
-     * NRF24 configuration holder class
+     * Configuration holder class
      */
     class Configuration
     {
